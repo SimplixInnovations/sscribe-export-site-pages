fed77d4efbba4756faf30d5799ba0df54f5e11a3
Simplix Innovations <info@simplixi.com>
2026-05-24 00:16:42 +0400

    Fix NonPrefixedVariable: rename export_index to sscribe_export_index
    WP has strict prefix requirements - all global variables in partials
    must use the plugin prefix to avoid conflicts with other plugins/themes.

--- admin/class-sscribe-admin.php
+++ admin/class-sscribe-admin.php
@@ -510,13 +510,13 @@ class SScribe_Admin {
 		$upload_dir = wp_upload_dir();
 		$export_dir = trailingslashit( $upload_dir['basedir'] ) . 'sscribe-exports/';
 
-		$export_index = get_option( 'sscribe_export_index', array() );
-		if ( ! is_array( $export_index ) ) {
-			$export_index = array();
+		$sscribe_export_index = get_option( 'sscribe_export_index', array() );
+		if ( ! is_array( $sscribe_export_index ) ) {
+			$sscribe_export_index = array();
 		}
 
-		$sscribe_recent_exports = ! empty( $export_index )
-			? $this->build_recent_exports( $export_index, $export_dir, $sscribe_wpml_active, $sscribe_languages, get_current_user_id() )
+		$sscribe_recent_exports = ! empty( $sscribe_export_index )
+			? $this->build_recent_exports( $sscribe_export_index, $export_dir, $sscribe_wpml_active, $sscribe_languages, get_current_user_id() )
 			: array();
 
 		include SSCRIBE_PLUGIN_DIR . 'admin/partials/sscribe-admin-display.php';
